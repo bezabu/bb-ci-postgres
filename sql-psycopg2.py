7f5b3248659646a4c456b167baa277cdf83ba6c3
bezabu <>
2024-01-23 15:50:22 +0000

    CRUD with ORM

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -1,27 +1,28 @@
 import psycopg2
 
+
 # connect to "chinook" database
 connection = psycopg2.connect(database="chinook")
 
-# build a cursor obect of the database
+# build a cursor object of the database
 cursor = connection.cursor()
 
-# query 1 select all recors from Artist table
+# Query 1 - select all records from the "Artist" table
 # cursor.execute('SELECT * FROM "Artist"')
 
-# query 2 select only the name column from the artist table
+# Query 2 - select only the "Name" column from the "Artist" table
 # cursor.execute('SELECT "Name" FROM "Artist"')
 
-# query 3
+# Query 3 - select only "Queen" from the "Artist" table
 # cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
 
-# query 4
+# Query 4 - select only by "ArtistId" #51 from the "Artist" table
 # cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
 
-# query 5
+# Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
 # cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
 
-# query 6
+# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
 cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
 
 # fetch the results (multiple)
